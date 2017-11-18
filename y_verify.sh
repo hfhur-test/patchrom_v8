54a501cbaebda67407f66255ce8913573279d924
hfhur <hfhur@163.com>
2017-11-18 18:54:00 +0800

    Revert "first commit"
    This reverts commit 2e5d8fffc1da807c2c90dcc314f5445169df8952.

--- y_verify.sh
+++
@@ -1,9 +0,0 @@
-adb remount
-rm out/framework* -rf
-rm out/android.policy* -rf
-rm out/services* -rf
-#make clean
-make out/framework.jar-phone
-make out/android.policy.jar-phone
-make out/services.jar-phone
-adb reboot
